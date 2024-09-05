5cd5a9e8b7804685ed0638ef1f6fdcf47ed38e7e
vinicius.coimbra <vinicius.coimbra@DDB0R9ZH3>
2024-09-05 11:16:36 -0300

    change text maind class

--- src/main/java/com/mappingmind/MappingMindBackApplication.java
+++ src/main/java/com/mappingmind/MappingMindBackApplication.java
@@ -8,6 +8,8 @@ public class MappingMindBackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MappingMindBackApplication.class, args);
+		
+		System.out.println("Hello World Jenkins");
 	}
 
 }
